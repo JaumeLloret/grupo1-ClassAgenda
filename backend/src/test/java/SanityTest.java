56fdc04bdf655a7dd5f220d2831de62dc4ac93ef
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-02-25 11:57:13 +0100

    añadir archivo Sanitytest

---
+++ backend/src/test/java/SanityTest.java
@@ -0,0 +1,13 @@
+package com.classagenda;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SanityTest {
+
+    @Test
+    void sanity() {
+        // Esta prueba solo sirve para verificar que JUnit y Maven funcionan
+        assertEquals(2, 1 + 1);
+    }
+}
